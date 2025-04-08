5796b674928fd9786dff44b418e96a71127c644f
toanehihi <hocongtoan987@gmail.com>
2025-04-08 22:13:20 +0700

    idk

--- src/components/hospital-manager/sidebar.tsx
+++ src/components/hospital-manager/sidebar.tsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { BarChart3, Users, UserRound } from 'lucide-react';
+import { BarChart3, Users, Network, Wallet, CalendarCheck2, HeartHandshake, Hospital } from 'lucide-react';
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -11,19 +11,39 @@ export function HospitalManagerSidebar({ className }: SidebarProps) {
   
   const routes = [
     {
-      title: "Dashboard",
+      title: "Tổng quan",
       href: "/hospital",
       icon: BarChart3,
     },
     {
-      title: "Staff",
-      href: "/hospital/staff",
+      title: "Thông tin bệnh viện",
+      href: "/hospital/information",
+      icon: Hospital,
+    },
+    {
+      title: "Khoa",
+      href: "/hospital/faculties",
+      icon: Network,
+    },
+    {
+      title: "Bác sĩ",
+      href: "/hospital/doctors",
       icon: Users,
     },
     {
-      title: "Patients",
-      href: "/hospital/patients",
-      icon: UserRound,
+      title: "Dịch vụ",
+      href: "/hospital/services",
+      icon: HeartHandshake,
+    },
+    {
+      title: "Lịch hẹn",
+      href: "/hospital/appointments",
+      icon: CalendarCheck2,
+    },
+    {
+      title: "Doanh thu",
+      href: "/hospital/incomes",
+      icon: Wallet,
     },
   ];
 
@@ -32,7 +52,7 @@ export function HospitalManagerSidebar({ className }: SidebarProps) {
       <div className="space-y-4 py-4">
         <div className="px-4 py-2">
           <h2 className="mb-2 px-2 text-xl font-semibold tracking-tight">
-            Hospital Management
+            Quản lý bệnh viện
           </h2>
           <ScrollArea className="h-[calc(100vh-10rem)]">
             <div className="space-y-2">
@@ -44,7 +64,7 @@ export function HospitalManagerSidebar({ className }: SidebarProps) {
                   className="w-full justify-start text-base py-6"
                   asChild
                 >
-                  <Link to={route.href}>
+                  <Link to={route.href} className="text-foreground">
                     <route.icon className="mr-3 h-5 w-5" />
                     {route.title}
                   </Link>
