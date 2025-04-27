e4bb4f5418dd7b884e88640ec769af5893ef8a1f
Lezh1n <hoangphilong1208@gmail.com>
2025-04-27 22:11:00 +0700

    refactor axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,10 @@
-import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
+import axios from "axios";
 import { API_BASE_URL } from "./constants";
-
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
     headers: {
-        'Content-Type': 'appication/json',
+        'Content-Type': 'application/json',
     },
     timeout: 10000,
 });
